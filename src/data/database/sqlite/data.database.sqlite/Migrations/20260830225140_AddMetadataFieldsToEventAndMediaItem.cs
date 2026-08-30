0168e45bddbdcb2432cb4f46a3688f71cd4861ff
rsperry79 <rsperry79@gmail.com>
2026-08-30 15:52:30 -0700

    migration: Add metadata fields to MediaItem table

---
+++ src/data/database/sqlite/data.database.sqlite/Migrations/20260830225140_AddMetadataFieldsToEventAndMediaItem.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VideoForensics.Data.Database.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMetadataFieldsToEventAndMediaItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApiSourceHash",
+                table: "MediaItems",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MetadataJson",
+                table: "MediaItems",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiSourceHash",
+                table: "MediaItems");
+
+            migrationBuilder.DropColumn(
+                name: "MetadataJson",
+                table: "MediaItems");
+        }
+    }
+}
